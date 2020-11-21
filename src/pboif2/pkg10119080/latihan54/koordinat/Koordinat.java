0a4efc7c542f06975edd1461c2eab7b8a78886e6
PratamaVin7 <kevinpratamaoke@gmail.com>
2020-11-21 22:21:45 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan54/koordinat/Koordinat.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan54.koordinat;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * (OBJECT ORIENTED) Koordinat
+ */
+public class Koordinat {
+    protected int x, y;
+    
+    public Koordinat(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+}
---
+++ src/pboif2/pkg10119080/latihan54/koordinat/PBOIF210119080Latihan54Koordinat.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan54.koordinat;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * (OBJECT ORIENTED) Koordinat
+ */
+public class PBOIF210119080Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat warnaKoordinat;
+        
+        warnaKoordinat = new WarnaKoordinat(10, 4, "Jingga");
+        System.out.println("Warna Koordinat : " + warnaKoordinat.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : " + warnaKoordinat.getX() + ", y "
+                           + ": " + warnaKoordinat.getY());
+    }
+    
+}
